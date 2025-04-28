ac7a657e989d15d176d07241cfdb3c7185cdad22
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-04-28 22:07:33 +0200

    stripped down to JDBC-Test

--- src/test/java/org/svenehrke/demo/springflywayjooq/DbTest.java
+++ src/test/java/org/svenehrke/demo/springflywayjooq/DbTest.java
@@ -3,16 +3,16 @@ package org.svenehrke.demo.springflywayjooq;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@JdbcTest
+@ContextConfiguration(classes = {TestcontainersConfiguration.class})
 @RequiredArgsConstructor
 class DbTest {
 
